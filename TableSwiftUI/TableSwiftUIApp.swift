9d90ef7830c919ce1bff9273999e940db44007ed
kituNew <vlad640vlad@gmail.com>
2025-02-11 14:34:37 +0300

    Initial Commit

---
+++ TableSwiftUI/TableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableSwiftUIApp.swift
+//  TableSwiftUI
+//
+//  Created by Zaitsev Vladislav on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TableSwiftUITests/TableSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TableSwiftUITests.swift
+//  TableSwiftUITests
+//
+//  Created by Zaitsev Vladislav on 11.02.2025.
+//
+
+import Testing
+@testable import TableSwiftUI
+
+struct TableSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
